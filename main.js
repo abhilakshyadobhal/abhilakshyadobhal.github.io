84c59d01d848a3bde93208aae00296185166bcf2
abhilakshyadobhal <abhilakshyadobhal@gmail.com>
2019-07-10 01:08:14 +0530

    internal links added

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-$(document).ready(function () {
-    $('ul.navbar-nav > li').click(function (e) {
-        e.preventDefault();
-        $('ul.navbar-nav > li').removeClass('active');
-        $(this).addClass('active');
-    });
+$(".navbar-nav a").on('click', function () {
+    $('.navbar-nav').find('li.active').removeClass('active');
+    $(this).parent('li').addClass('active');
 });
+// $(document).ready(function () {
+//     $('ul.navbar-nav > li').click(function (e) {
+//         e.preventDefault();
+//         $('ul.navbar-nav > li').removeClass('active');
+//         $(this).addClass('active');
+//     });
+// });
 
 // MDB Lightbox Init
 $(function () {
